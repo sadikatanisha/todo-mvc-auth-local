bff382e62353603ae617654e123c0f1359650e78
sadikatanisha <sadikatanisha@gmail.com>
2023-08-27 14:28:09 +0600

    initial commit

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,9 +2,10 @@ const Todo = require('../models/Todo')
 
 module.exports = {
     getTodos: async (req,res)=>{
-        console.log(req.user)
+        console.log(req.user.id)
         try{
             const todoItems = await Todo.find({userId:req.user.id})
+            
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
             res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
         }catch(err){
